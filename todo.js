5519d977538072d91a7a088eb59357fb219280cb
Jonathan Britton <jbritttt29@hotmail.com>
2024-01-27 18:01:46 +0000

    todo list

---
+++ todo.js
@@ -0,0 +1,14 @@
+// Timer value needs to restore on page refresh or window reopen
+
+//save timer value in local storage.
+// create an object each counter variable on every interval.
+// push object into localArray
+// save to local storage on every interval.
+
+
+// Functionality needed to enter a custom time
+
+// created input fields for each counter variable and set a range
+// update timer on click
+// push to localArray and save in local storage
+
