9f52693c8b07e3d1e4cc446f84cd9f4d8befcac0
Jonathan Britton <jbritttt29@hotmail.com>
2024-02-04 19:54:10 +0000

    Todos

--- todo.js
+++ todo.js
@@ -1,27 +1,6 @@
 // Timer value needs to restore on page refresh or window reopen
 
-//save timer value in local storage.
-// create an object each counter variable on every interval.
-// push object into localArray
-// save to local storage on every interval.
-
-
-// Functionality needed to enter a custom time
-
-// created input fields for each counter variable and set a range
-// update timer on click
-// push to localArray and save in local storage
-
-
-// create functionality to delete all stamps from local storage
-
-
-// fix bugs
-
-// play button can be clicked multiple times which starts a new interval
-// delete modal doesnt show after generating a new stamp.
-
-
+// Event listener code is repeated many times. Refactor by calling the event listeners by invoking them.
 
 
 //Use puppeteer to grab recording timer value from ms teams
